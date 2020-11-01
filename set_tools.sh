0a5fc1c1b463a1f874298538008e5580d64f1b52
ImZeus0 <48887400+ImZeus0@users.noreply.github.com>
2020-11-01 22:00:50 +0200

    Update set_tools.sh

--- set_tools.sh
+++ set_tools.sh
@@ -2,6 +2,7 @@
 apt install curl -y
 apt install dante-server -y
 pip3 install requests
+pip install requests
 chmod +x /etc/init.d/danted
 update-rc.d danted defaults
 ip a > setup_proxy/interfase.txt
